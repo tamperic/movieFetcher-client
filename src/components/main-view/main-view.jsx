c430e7f982bd78dce6b9ff9e8745c97f0e028600
tamperic <tamaraperic.work@gmail.com>
2025-03-16 15:06:58 +0100

    Add useEffect

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,51 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MovieView } from "../movie-view/movie-view";
 import { MovieCard } from "../movie-card/movie-card";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Casino Royale",
-      description: "After earning a licence to kill, secret agent James Bond sets out on his first mission as 007. Bond must defeat a private banker funding terrorists in a high-stakes game of poker at Casino Royale, in Montenegro.",
-      genre: "Action",
-      director: "Martin Campbell",
-      rating: 8.0,
-      releaseYear: 2006,
-      duration: "2h 24m",
-      actors: ["Daniel Craig", "Eva Green", "Mads Mikkelsen"],
-      image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRa1z0r77Zs8QNqQI29PKKqc1q433AQH22MGi1Qgazn-BeyOalU",
-      featured: false
-    },
-    {
-      id: 2,
-      title: "Ice Age",
-      description: "Manny the mammoth, Sid the loquacious sloth, and Diego the sabre-toothed tiger go on a comical quest to return a human baby back to his father, across a world on the brink of an ice age.",
-      genre: "Adventure",
-      director: "Chris Wedge",
-      rating: 7.5,
-      releaseYear: 2002,
-      duration: "1h 21m",
-      actors: ["Ray Romano", "John Leguizamo", "Denis Leary"],
-      image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT0G0riEkCZr9DXXBkKqhaxwPXya9dARB0BdGVWedML3eY3i_Xh",
-      featured: false
-    },
-    {
-      id: 3,
-      title: "The Eight Hundred",
-      description: "In 1937, 800 Chinese soldiers fight under siege from a warehouse in the middle of the Shanghai battlefield, completely surrounded by the Japanese army.",
-      genre: "War",
-      director: "Guan Hu",
-      rating: 6.7,
-      releaseYear: 2020,
-      duration: "2h 29m",
-      actors: ["Zhizhong Huang", "Junyi Zhang", "Hao Ou"],
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQovgv0Y9G1olcX4OXbEQzYarzxhlNzB4jA7U_NxEUBwm_uhIZK",
-      featured: true
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  // Synchronize a component with an external system
+  useEffect(() => {
+    fetch("https://movie-fetcher-5a8669cd2c54.herokuapp.com/movies")
+    .then((response) => response.json())
+    .then((data) => {
+      const moviesFromApi = data.map((movie) => {
+        return {
+          ...movie,
+          id: movie._id
+        };
+      });
+      console.log("testttt", moviesFromApi);
+      setMovies(moviesFromApi); // Update the state of the component
+    });
+  }, []);
 
   if(selectedMovie) {
     return (
@@ -64,7 +40,7 @@ export const MainView = () => {
           key={movie.id}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
-          setSelectedMovie(newSelectedMovie);
+            setSelectedMovie(newSelectedMovie);
           }}
         />
       ))}
