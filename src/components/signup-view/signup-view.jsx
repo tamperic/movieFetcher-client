59687e4433faa237326f3b99a5d0b55f5a0611b7
tamperic <tamaraperic.work@gmail.com>
2025-04-05 19:54:29 +0200

    Import useNavigate

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -2,39 +2,41 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import PropTypes from 'prop-types';
+import { useNavigate } from "react-router";
 
 export const SignupView = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [birthDate, setBirthDate] = useState("");
+  const navigate = useNavigate();
 
-const handleSubmit =(event) => {
-  event.preventDefault(); // Prevents the default behavior (to reload the entire page) of the form
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevents the default behavior (to reload the entire page) of the form
 
-  const data = {
-    username: username,
-    password: password,
-    email: email,
-    birthDate: birthDate
+    const data = {
+      username: username,
+      password: password,
+      email: email,
+      birthDate: birthDate
+    };
+
+    fetch("https://movie-fetcher-5a8669cd2c54.herokuapp.com/users", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then((response) => {
+      if (response.ok) {
+        alert("Singup successful!");
+        navigate("/login");
+      } else {
+        alert("Signup failed.");
+      }
+    });
   };
-
-  fetch("https://movie-fetcher-5a8669cd2c54.herokuapp.com/users", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-  .then((response) => {
-    if (response.ok) {
-      alert("Singup successful!");
-      window.location.reload(); // Reload the page so that user can log in with their newly created account
-    } else {
-      alert("Signup failed.");
-    }
-  });
-};
 
   // onSubmit tells the login API to validate username, password, email, and birthDate
   return (
@@ -84,6 +86,8 @@ const handleSubmit =(event) => {
       <Button variant="primary" type="submit">Submit</Button>
     </Form>
   );
+};
+
 SignupView.propTypes = {
   user: PropTypes.shape({
     username: PropTypes.string.isRequired,
