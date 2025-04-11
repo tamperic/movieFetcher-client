4b6bc487e17247a5a7c038f2dbf4dbe19aec5250
tamperic <tamaraperic.work@gmail.com>
2025-04-11 22:21:18 +0200

    Update navbar styling

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 export const NavigationBar = ({ user, onLoggedOut }) => {
   return (
     <Navbar expand="lg" className="w-100 mb-4">
-      <Container>
+      <Container className="custom-navbar-collapse">
         <Navbar.Brand as={Link} to="/">MovieFetcher</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" >
           <Nav className="me-auto">
             {!user && (
               <>
